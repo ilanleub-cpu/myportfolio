f5ebde0e629cd6e89a4db3506703cdaf1f2f9ae9
Ilan Tamir Leubitz <iganileubitz@gmail.com>
2025-10-02 15:53:59 -0700

    Add minimal Next.js index page

---
+++ myportfolio/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>MyPortfolio app is deploying...</h1>
+      <p>If you see this page, the full app will be available after the build.</p>
+    </div>
+  );
+}
